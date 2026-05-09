ea223f55bf0258116a75d6afc70c2c05115edd24
OdinTheScientist <vikingsciencegaming@gmail.com>
2026-05-09 00:57:39 -0300

    Add CLAUDE.md and clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get "techniques/index"
-  get "techniques/show"
-  get "techniques/new"
-  get "techniques/edit"
-  get "positions/index"
-  get "positions/show"
-  get "positions/new"
-  get "positions/edit"
   root "positions#index"
-
   resources :positions
   resources :techniques
+  resources :resources
 end
